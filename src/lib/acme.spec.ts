a4aed08e1da4d158a5dd6743afd0ec4920e40c33
Wandu <31390092+sMiter911@users.noreply.github.com>
2024-05-28 17:23:15 +0200

    Initial commit

---
+++ src/lib/acme.spec.ts
@@ -0,0 +1,7 @@
+import { acme } from './acme';
+
+describe('acme', () => {
+  it('should work', () => {
+    expect(acme()).toEqual('acme');
+  });
+});
---
+++ src/lib/acme.ts
@@ -0,0 +1,3 @@
+export function acme(): string {
+  return 'acme';
+}
